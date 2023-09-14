15d75fcfc6d5f126c833b82636e1336ae82a9a72
Snowail <45956036+Snowail@users.noreply.github.com>
2023-09-14 21:53:34 +0800

    修复

--- lnmp_ubuntu.sh
+++ lnmp_ubuntu.sh
@@ -59,10 +59,9 @@ mkdir /etc/nginx/global
 # 创建CloudFlare白名单
 wget -O /etc/nginx/global/cf_ipv4.txt https://www.cloudflare.com/ips-v4
 wget -O /etc/nginx/global/cf_ipv6.txt https://www.cloudflare.com/ips-v6
-
-cat cf_ipv4.txt > cf.conf && cat cf_ipv6 >> cf.conf
-cat > /etc/nginx/global/cf.conf << EOF
-sed -i "s/^/allow /g" cf.conf
+cat /etc/nginx/global/cf_ipv4.txt > /etc/nginx/global/cf.conf 
+cat /etc/nginx/global/cf_ipv6 >> /etc/nginx/global/cf.conf
+sed -i "s/^/allow /g" /etc/nginx/global/cf.conf
 
 # 其他配置
 wget -O /etc/nginx/global/fastcgi_timeout.conf https://raw.githubusercontent.com/Snowail/ScriptsCollection/main/nginx_global/fastcgi_timeout.conf?token=GHSAT0AAAAAACHSZY73Z5CK5FAWXR4JDIPYZIDBYCQ
